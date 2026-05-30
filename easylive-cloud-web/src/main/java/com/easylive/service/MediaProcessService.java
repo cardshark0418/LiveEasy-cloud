d6b53b927c75a0235994fcc01bce145efa985695
cardshark0418 <472019579@qq.com>
2026-05-30 18:31:15 +0800

    微服务

--- easylive-cloud-web/src/main/java/com/easylive/service/MediaProcessService.java
+++ easylive-cloud-web/src/main/java/com/easylive/service/MediaProcessService.java
@@ -1,5 +1,5 @@
 package com.easylive.service;
 
 public interface MediaProcessService {
-    public String extractAudioFromM3u8(String m3u8FilePath);
+    String extractAudioFromM3u8(String m3u8FilePath);
 }
